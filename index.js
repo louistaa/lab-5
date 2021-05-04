a763a963e6a6d55820798d9e64263ac0584af226
Louis Ta <louistaa@gmail.com>
2021-05-03 18:03:59 -0700

    fetch and json

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-// one object containing an array of objects, exactly how JSON is structured
-let data = {
-  places: [
-    {id: 1, name: "McDonald's", stars: 4},
-    {id: 2, name: "Shake Shack", stars: 5},
-    {id: 3, name: "In and Out", stars: 5},
-    {id: 4, name: "Arby's", stars: 3},
-    {id: 5, name: "Wendy's", stars: 4},
-    {id: 6, name: "Jack In the Box", stars: 2},
-    {id: 7, name: "iHop", stars: 1},
-  ]
-}
+// keep track of state
+let state = {
+  data: {}
+};
+
+fetch('data.json')
+  .then((response) => { // download
+    return response.json();
+  })
+  .then((downloadedData) => { // encoding
+    state.data = downloadedData; // when the data is ready, assign state.data to the downloaded data
+    console.log(state.data); // sanity check to see if our data has been downloaded successfully
+    // remove console log before you turn in your project
+
+    // render cards after data has been loaded into state
+    renderCards();
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
 // create a single card using vanilla js
 function createCard(aSinglePlace) {
@@ -44,9 +52,7 @@ function renderCards() {
   let linkToRow = document.querySelector('#feed')
   linkToRow.innerHTML = "";
 
-  data.places.forEach(function(place) {
+  state.data.places.forEach(function(place) {
     linkToRow.appendChild(createCard(place));
   });
 }
-
-renderCards(); // default view
